7c24e562a6119eb73b8aeba33c7a681b39a96b97
Codesessor2000 <chaitanya.malegaonkar@gmail.com>
2024-05-06 13:20:08 +0530

    JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "Chaitanya$Learning$MERN"
 
 //Create a user using POST "/api/auth/createUser". DOesn't require auth. No Login required
 router.post('/createUser', [
@@ -20,12 +24,20 @@ router.post('/createUser', [
         if (user) {
             return res.status(400).json({ errors: "email already exists" });
         }
+        const salt = await bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
-        })
-        res.json(user);
+            password: secPass
+        });
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({authToken});
     } catch (error) {
         console.error(error.message)
         res.status(500).send("Internal server error");
