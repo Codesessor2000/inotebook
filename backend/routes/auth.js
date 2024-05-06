ca1e137004b2952d59a0fe30294c8682dd5f406d
Codesessor2000 <chaitanya.malegaonkar@gmail.com>
2024-05-06 08:35:05 +0530

    some db change and createUser api changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-//Create a user using POST "/api/auth". DOesn't require auth
-router.post('/',(req,res)=>{
+//Create a user using POST "/api/auth/createUser". DOesn't require auth. No Login required
+router.post('/createUser', [
+    body('name', 'Password must be atleast 3 characters').isLength({ min: 3 }),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+    body('email', 'Enter a valid email').isEmail()
+], async (req, res) => {
     console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        //check if email exists already
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ errors: "email already exists" });
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json(user);
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal server error");
+    }
 })
 module.exports = router;
